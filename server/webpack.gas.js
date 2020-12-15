3f161958b749e0ef5fbba80d2b22b25624f54e58
chiachenghuang <suzuke@hotmail.com>
2020-12-15 14:39:29 +0800

    declaring core-js version in @babel/preset-env

--- server/webpack.gas.js
+++ server/webpack.gas.js
@@ -26,7 +26,8 @@ module.exports = {
                 [
                   '@babel/preset-env',
                   {
-                    useBuiltIns: 'usage'
+                    useBuiltIns: 'usage',
+                    corejs: "2.6.12",
                   }
                 ]
               ]
